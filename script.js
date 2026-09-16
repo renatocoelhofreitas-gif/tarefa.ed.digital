e8784831ecb926b3c5c8178adf8d337e17db197a
renatocoelhofreitas-gif <renato.coelho.freitas@escola.pr.gov.br>
2026-09-16 13:48:19 -0300

    a tualizei script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const botoesCurtir = document . querselectorAl(".curtir");
+botoesCurtir.forEach(function(botaoCurtir) {
+let curtir = false;
+botaoCurtir. addEventListener("click", curtir);
+function curtir()})
+const contador = botaoCurtir. queryselector("span");
+if(curtiu===false){
+    contador.texContent++;
+    curtir= true;}
+    else{
+        contador.texContent--;
+        curtiu= false;
+    }
